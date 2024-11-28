e242e1a5b754fcfc620dae18da790e0571d87afd
bmkriszti <147618830+bmkriszti@users.noreply.github.com>
2024-11-28 18:04:31 +0100

    Testing development

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Simple route
 @app.route('/')
 def home():
-    return "Hello, World!!!:)"
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
